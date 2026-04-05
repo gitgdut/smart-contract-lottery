97978b551f5a0d1c76fbc598eee7279726fe1235
bodier <threebodybreaker@gmail.com>
2026-04-05 16:14:59 +0800

    Save progress before installing solmate

---
+++ test/uint/RaffleTest.t.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+
+contract RaffleTest is Test {
+    Raffle public raffle;
+    HelperConfig public helperConfig;
+    uint256 entranceFee;
+    uint256 interval;
+    address vrfCoordinator;
+    bytes32 gasLane;
+    uint32 callbackGasLimit;
+    uint256 subscriptionId;
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
+    event RaffleEntered(address indexed player);
+    event WinnerPicked(address indexed winner);
+
+    function setUp() external {
+        DeployRaffle deployer = new DeployRaffle();
+        (raffle, helperConfig) = deployer.deployContract();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        entranceFee = config.entranceFee;
+        interval = config.interval;
+        vrfCoordinator = config.vrfCoordinator;
+        gasLane = config.gasLane;
+        callbackGasLimit = config.callbackGasLimit;
+        subscriptionId = config.subscriptionId;
+        vm.deal(PLAYER, STARTING_PLAYER_BALANCE);
+    }
+
+    function testRaffleInitializesInOpenState() public view {
+        //assert
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+
+    function testRaffleRevertsWhenYouDontPayEnough() public {
+        //arrange
+        vm.prank(PLAYER);
+        //act/assert
+        vm.expectRevert(Raffle.Raffle__SendMoreToEnterRaffle.selector);
+
+        raffle.enterRaffle();
+    }
+    function testRaffleRecordsPlayerWhenTheyEnter() public {
+        //arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        //act
+        address playerRecorded = raffle.getPlayer(0);
+        //assert
+        assert(playerRecorded == PLAYER);
+    }
+    function testEnteringRaffleEmitsEvent() public {
+        //arrange
+        vm.prank(PLAYER);
+        //act
+        vm.expectEmit(true, false, false, false, address(raffle));
+        emit RaffleEntered(PLAYER);
+        //assert
+        raffle.enterRaffle{value: entranceFee}();
+    }
+    function testDontAllowPlayersToEnterWhileRaffleIsCalculating() public {
+        //arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep("");
+        //act/assert
+        vm.expectRevert(Raffle.Raffle__RaffleNotOpen.selector);
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+    }
+}
